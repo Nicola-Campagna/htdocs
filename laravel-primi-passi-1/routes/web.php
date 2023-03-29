035023ce72155f11d66c90e5b6903ca81fa91d9a
Nicola-Campagna <campagnanicola379@gmail.com>
2023-03-29 16:12:25 +0200

    hello world

--- laravel-primi-passi-1/routes/web.php
+++ laravel-primi-passi-1/routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $hello="HELLO WORLD !";
+    return view('home',compact("hello"));
+})->name("homepage");
